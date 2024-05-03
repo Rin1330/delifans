6d4ee839255aac690f32e2f14861f2091ee22c91
sakimotorin <sakimotorin@gmail.com>
2024-05-03 17:56:31 +0800

    update header nav-link transition style and swiper button style

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,1 +1,24 @@
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap"
+import Swiper from "swiper"
+import { Navigation, Pagination } from "swiper/modules"
+
+const swiper = new Swiper("#home-banner", {
+	modules: [Navigation, Pagination],
+	// Optional parameters
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: "#home-banner .swiper-pagination",
+		clickable: true,
+		renderBullet: function (index, className) {
+			return `<a class="${className}"><span class="fs-0">${index + 1}</span></a>`
+		},
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: "#home-banner .swiper-button-next",
+		prevEl: "#home-banner .swiper-button-prev",
+	},
+})
--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss"
 
-import './assets/js/all.js';
+import "./assets/js/all.js"
