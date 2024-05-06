a7ef69256665a9eb45cd00d5ca63a68bec65e8c9
sakimotorin <sakimotorin@gmail.com>
2024-05-06 17:48:24 +0800

    create home banner

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,18 +1,21 @@
 import * as bootstrap from "bootstrap"
 import Swiper from "swiper"
-import { Navigation, Pagination } from "swiper/modules"
+import { Navigation, Pagination, Autoplay } from "swiper/modules"
 
 const swiper = new Swiper("#home-banner", {
-	modules: [Navigation, Pagination],
-	// Optional parameters
+	modules: [Navigation, Pagination, Autoplay],
 	loop: true,
-
-	// If we need pagination
+	loopedSlides: 5,
+	slidesPerView: "auto",
+	autoplay: {
+		delay: 2500,
+	},
+	speed: 2000,
 	pagination: {
 		el: "#home-banner .swiper-pagination",
 		clickable: true,
 		renderBullet: function (index, className) {
-			return `<a class="${className}"><span class="fs-0">${index + 1}</span></a>`
+			return `<a class="${className} rounded-3"><span class="fs-0">${index + 1}</span></a>`
 		},
 	},
 
